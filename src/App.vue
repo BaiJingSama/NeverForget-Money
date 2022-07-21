d936c5084e1bc98e84f5ddd2a5029bb1a7cd2791
Jackeybai <1303802862@qq.com>
2022-07-21 16:45:57 +0800

    Money页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <router-view /><!-- 内容要显示在这里 -->
-    <hr>
   </div>
 </template>
 
 <style lang="scss">
+// App.vue的style 不需要加scoped 因为就是需要影响全局
+// 一般加body样式都在App.vue里加
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="nav">
         <router-link to="/money">记账</router-link> |
         <router-link to="/labels">标签</router-link> |
         <router-link to="/statistics">统计</router-link> |
@@ -14,5 +14,12 @@ export default Vue.extend({
 })
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+// scoped 只在当前组件范围的样式
+// vue会在div上加一个data-v-xxx的属性
+// .nav[data-v-xxx{样式}]
+
+.nav {
+    border: 1px solid red
+}
 </style>
