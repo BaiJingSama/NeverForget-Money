06a08b4fb0063cbfc3a9e523b1e5c5e9deb4b58a
Jackeybai <1303802862@qq.com>
2022-07-21 19:37:52 +0800

    添加了布局 创建了Layout文件

---
+++ src/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- css重复所以用一个组件全局声明用Vue的插槽 把重要的内容插入进来 -->
+    <div class="nav-wrapper">
+        <div class="content">
+            <!-- 这个slot标签会被其他组件的Layout标签内的内容替换 -->
+            <slot />
+        </div>
+        <Nav />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'Layout'
+
+})
+</script>
+
+<style lang="scss" scoped>
+.nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh
+}
+
+.content {
+    flex-grow: 1;
+    overflow: auto;
+    // 上面部分如果内容太多导致超出页面 让这个content自己加滚动条不影响导航栏
+}
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-            Money.vue
-        </div>
-        <Nav />
+    <div>
+        <layout>
+            <p>Money.vue</p>
+        </layout>
     </div>
 </template>
 
@@ -16,16 +15,4 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-    border: 1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh
-}
-
-.content {
-    flex-grow: 1;
-    overflow: auto;
-    // 上面部分如果内容太多导致超出页面 让这个content自己加滚动条不影响导航栏
-}
 </style>
