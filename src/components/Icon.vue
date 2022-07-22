dfd582fb2caae0e7a508094fd6e3310fc8c4def4
Jackeybai <1303802862@qq.com>
2022-07-22 11:23:16 +0800

    创建并使用了Icon组件 <Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg class="icon">
+        <use v-bind:xlink:href="'#' + name" />
+        <!-- v-bind绑定把这个属性给外面可以编辑 -->
+    </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext)
+try { importAll(require.context('../assets/icons', true, /\.svg$/)) }
+catch (error) { console.log(error); }
+// 这两行代码可以把整个icons的svg文件都引入进来，记住这个API
+
+export default Vue.extend({
+    props: ['name']
+
+})
+</script>
+
+<style lang="scss" scoped>
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,36 @@
 <template>
     <div class="nav">
-        <router-link to="/money">记账</router-link> |
-        <router-link to="/labels">标签</router-link> |
-        <router-link to="/statistics">统计</router-link> |
+        <router-link to="/money">
+            <Icon name="money" />
+            记账
+        </router-link> |
+        <router-link to="/labels">
+            <Icon name="label" />
+            标签
+        </router-link> |
+        <router-link to="/statistics">
+            <Icon name="statistics" />
+            统计
+        </router-link> |
     </div>
 </template>
 
-<script lang="ts">
+<script lang=" ts">
 import Vue from 'vue'
+
+
 
 export default Vue.extend({
 
 })
 </script>
 
-<style lang="scss" scoped>
-// scoped 只在当前组件范围的样式
-// vue会在div上加一个data-v-xxx的属性
-// .nav[data-v-xxx{样式}]
-
-.nav {
-    border: 1px solid red
-}
-</style>
+                <style lang="scss" scoped>
+                // scoped 只在当前组件范围的样式
+                // vue会在div上加一个data-v-xxx的属性
+                // .nav[data-v-xxx{样式}]
+                
+                .nav {
+                    border: 1px solid red
+                }
+                </style>
